553f71846a8f4075faa3f759557217abdfe0d83c
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-21 15:39:17 +0100

    Update Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 import "../components/button.css";
+import Panda from "../assets/Panda.png";
+import "./style.css";
 
 function Home() {
   return (
-    <div>
-      <h1> Simply Delicious  </h1>
-      <h3> Welcome to our recipe book</h3>
-      <h4> Join the Simply Delicious- Where Cooking Meets Joy.</h4>
+    <div className="homepage">
+      <h1> Simply Delicious </h1>
+      <h2> Welcome to Our Recipe Sharing Platform!</h2>
+      <div className="flex">
+      <div className="content">
+      <p>Share your favorite recipes with the world or explore new ones from passionate chefs. </p>
+        <p>Whether you're a beginner or a seasoned cook, our platform has something to inspire everyone!.</p>
+        <p> Join the Simply Delicious- Where Cooking Meets Joy.</p>
+        <Link to="/recipes">
+          <button className="moving-border-box"> 🎋 Let's Get Started </button>
+        </Link>
 
-      <div><Link to = "/recipes">
-        <button className="moving-border-box"> 🎋 Let's Get Started </button></Link>
       </div>
+      <div className="hero">
+  <img src={Panda} alt="Panda" />
+</div>
+</div>
     </div>
   );
 }
