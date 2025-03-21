cff392b1a33d3637abf1e797d04a8a9180bf44ed
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-21 12:36:34 +0100

    Add Recipe page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path = "/recipes" element={<Recipes />} />
       </Routes>
     </>
   );
