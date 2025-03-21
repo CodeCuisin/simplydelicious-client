fbaee2105dc6fca37968b9e59a428dd5cabaefe0
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-21 12:36:09 +0100

    Add basic styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
-function Home (){
+import { Link } from "react-router-dom";
+import "../components/button.css";
 
+function Home() {
+  return (
+    <div>
+      <h1> Simply Delicious  </h1>
+      <h3> Welcome to our recipe book</h3>
+      <h4> Join the Simply Delicious- Where Cooking Meets Joy.</h4>
 
-    return(
-        <div>
-        <h1> Simply Delicious </h1>
-<h3> Welcome to our recipe book</h3>
-        </div>
-    )
+      <div><Link to = "/recipes">
+        <button className="moving-border-box"> 🎋 Let's Get Started </button></Link>
+      </div>
+    </div>
+  );
 }
 export default Home;
