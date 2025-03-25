89c5adcbfbf4c9c0f5b158981b8e2f69bbb8d36d
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-25 21:11:59 +0100

    Implement searchbar

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./sidebar.css";
+
+interface SearchbarProps {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+}
+
+const Searchbar: React.FC<SearchbarProps> = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <form className="search-bar" onSubmit={(e) => e.preventDefault()}>
+      <input
+        type="text"
+        placeholder="Search recipes..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default Searchbar;
