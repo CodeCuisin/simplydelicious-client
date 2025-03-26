c4f77b63418ed1f3f75429a646ceeeb298c4251f
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-26 18:19:25 +0100

    Limit the visibility of my profile page only if user is logged in.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useAuth } from "../context/auth.context";
-import "./sidebar.css";
 import simplydelicious from "../assets/Simplydelicious.gif";
 
 type SidebarItem = {
@@ -11,53 +10,58 @@ type SidebarItem = {
   onClick?: () => void;
 };
 
-
 const Sidebar: React.FC = () => {
-  const {userId} = useParams();
+  const { userId } = useParams();
   const { isLoggedIn, logOutUser } = useAuth();
   console.log(userId);
   const sidebarItems: SidebarItem[] = [
-
-    { label: "Login 👤", link: "/login", hideIfLoggedIn: true },
-    { label: "My Profile 👤", link: `/users/${userId}` },
-    { label: "Categories", link: "/categories" },
-    { label: "Popular Recipes 📓", link: "/popular" },
+    { label: "Home   🏠︎ ", link: "/recipes" },
+    { label: "Login  👤", link: "/login", hideIfLoggedIn: true },
+    { label: "Categories  📋", link: "/categories" },
+    { label: "Popular Recipes 🔥", link: "/popular" },
     { label: "Create Recipe ✚ ", link: "/create-recipe" },
   ];
 
-
   return (
-    <div className="bg-green-950 w-80 ">
-        <div className="gif-container mb-4 m-6">
+    <div className="bg-green-950 w-80 text-white p-2">
+      <div className="gif-container  m-4">
         <img
-          className="w-58 h-60 object-cover rounded-2xl"
-          src={simplydelicious} 
+          className="w-58 h-58 object-cover rounded-2xl"
+          src={simplydelicious}
           alt="Sidebar Animation"
           onError={() => alert("Error loading GIF!")}
         />
       </div>
-      <ul className="m-8 ">
-        <Link to="/recipes">
-          <button className="main"> Home 🏠︎</button>
+      <ul className="flex flex-col m-3 drop-shadow-md gap-10 ">
+        <Link to="">
+          <button> </button>
         </Link>
-        {sidebarItems.filter((item) => !(isLoggedIn && item.hideIfLoggedIn)).map((item, index) => (
-          <button key={index} className="Recipe">
-            {" "}
-              <Link className="sidebar-item" to={item.link} >
+        {sidebarItems
+          .filter((item) => !(isLoggedIn && item.hideIfLoggedIn))
+          .map((item, index) => (
+            <button className="bg-slate-900 p-2 rounded-lg" key={index}>
+              {" "}
+              <Link  to={item.link}>
                 {item.label}
               </Link>
-          </button>
-        ))}
-       {isLoggedIn && (
-          <li>
-            <button className="Recipe" onClick={logOutUser}>
-              Logout 👤
             </button>
-          </li>
+          ))}
+        {isLoggedIn && (
+        
+            <div >
+              <li className="flex flex-col bg-slate-900 p-2 rounded-lg text-center">
+            <Link  to ={`/users/${userId}`}>
+              <button onClick={logOutUser}>My Profile 👤</button>
+              </Link>
+              </li>
+             
+            <li>
+              <button className="flex flex-col bg-slate-900 p-2  mt-10 rounded-lg w-70" onClick={logOutUser}>Logout ⏻</button>
+              </li>
+              </div>
         )}
       </ul>
     </div>
-  
   );
 };
 
