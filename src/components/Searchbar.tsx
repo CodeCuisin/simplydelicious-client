39f49352505f78a208a7071e837b15fa532d760b
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-26 18:16:57 +0100

    Increase size of search icon

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -8,7 +8,8 @@ interface SearchbarProps {
 
 const Searchbar: React.FC<SearchbarProps> = ({ searchQuery, setSearchQuery }) => {
   return (
-    <form className="m-20" onSubmit={(e) => e.preventDefault()}>🔎
+    <form className="flex justify-center m-10" onSubmit={(e) => e.preventDefault()}>
+      <div className="text-3xl m-1">🔎</div>
       <input className="border-2 border-double w-100 h-10 p-5 text-xl "
         type="text"
         placeholder="Search recipes..."
