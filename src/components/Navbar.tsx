153e941509b70410e2bba570102b901db4bdafd3
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-05-06 11:46:15 +0200

    Adjust title to the page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar: React.FC = () => {
 
 return(
     <div className="bg-green-950 w-410 h-25">
-<h1 className="text-6xl italic text-center text-white p-2"> Simply delicious</h1>
+<h1 className="text-6xl italic ml-100 text-white p-2 "> Simply delicious</h1>
 </div>
 )
 }
